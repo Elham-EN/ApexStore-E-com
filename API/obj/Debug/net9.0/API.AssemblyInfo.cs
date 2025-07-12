288ed6158ef6e3d5f360a9455b1188efb6b740aa
Elham-EN <ilhamrezaie@gmail.com>
2025-07-13 00:40:36 +1000

    executed migration to create the database

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3120f04488716c6bce0a5c859bebf3da0afff07a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a89a02f12ab58b93ec8ec51a0edd358064935259")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
