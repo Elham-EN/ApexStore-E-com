426aa0a436f792abecc572f31acf60eeae8426dd
Elham-EN <ilhamrezaie@gmail.com>
2025-07-13 23:54:28 +1000

    Boostraped Client App React.js + Vite

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1 style={{ fontFamily: "sans-serif", fontSize: "2.5rem" }}>
+        React Client App: ApexStore
+      </h1>
+    </div>
+  );
+}
+
+export default App;
