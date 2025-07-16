e9a2a1273a859129800326c94273c746546a6376
Elham-EN <ilhamrezaie@gmail.com>
2025-07-16 23:14:08 +1000

    added links to navbar and styles

--- client/src/app/layout/Navbar.tsx
+++ client/src/app/layout/Navbar.tsx
@@ -1,24 +1,98 @@
 import React from "react";
-import { AppBar, Toolbar, Typography } from "@mui/material";
+import {
+  AppBar,
+  Badge,
+  Box,
+  IconButton,
+  List,
+  ListItem,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import { useColorScheme } from "@mui/material/styles";
 import MaterialUISwitch from "@/app/components/ThemeSwitch";
+import { NavLink } from "react-router";
+import { ShoppingCart } from "@mui/icons-material";
 
 export default function Navbar(): React.ReactElement {
   const { mode, setMode } = useColorScheme();
+
+  const midLinks = [
+    { title: "catalog", path: "/catalog" },
+    { title: "about", path: "/about" },
+    { title: "policy", path: "/store-policy" },
+  ];
+
+  const rightLinks = [
+    { title: "login", path: "/login" },
+    { title: "register", path: "/register" },
+  ];
+
+  const navLinkStyles = {
+    color: "inherit",
+    typography: "h6",
+    textDecoration: "none",
+    "&:hover": { color: "grey.500" },
+    "&.active": { color: "#baecf9" },
+  };
 
   const handleThemeChange = () => {
     setMode(mode === "light" ? "dark" : "light");
   };
   return (
     <AppBar position="fixed">
-      <Toolbar sx={{ justifyContent: "space-between" }}>
-        <Typography variant="h5">ApexStore</Typography>
-        <div>
+      <Toolbar
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <Box sx={{ display: "flex", alignItems: "center" }}>
+          <Typography
+            component={NavLink}
+            to={"/"}
+            variant="h5"
+            sx={{ textDecoration: "none", color: "inherit" }}
+          >
+            ApexStore
+          </Typography>
           <MaterialUISwitch
             checked={mode === "dark"}
             onChange={handleThemeChange}
           />
-        </div>
+        </Box>
+        <List sx={{ display: "flex" }}>
+          {midLinks.map(({ title, path }) => (
+            <ListItem
+              component={NavLink}
+              to={path}
+              key={path}
+              sx={navLinkStyles}
+            >
+              {title.toUpperCase()}
+            </ListItem>
+          ))}
+        </List>
+        <Box sx={{ display: "flex", gap: 2 }}>
+          <IconButton sx={{ color: "inherit" }}>
+            <Badge badgeContent="4" color="secondary">
+              <ShoppingCart />
+            </Badge>
+          </IconButton>
+          <List sx={{ display: "flex" }}>
+            {rightLinks.map(({ title, path }) => (
+              <ListItem
+                component={NavLink}
+                to={path}
+                key={path}
+                sx={navLinkStyles}
+              >
+                {title.toUpperCase()}
+              </ListItem>
+            ))}
+          </List>
+        </Box>
       </Toolbar>
     </AppBar>
   );
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,14 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./app/styles.css";
+import { RouterProvider } from "react-router";
+import "@/app/styles.css";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import { CssBaseline, ThemeProvider } from "@mui/material";
-import { theme } from "./app/Theme";
-import { RouterProvider } from "react-router";
-import { router } from "./app/routes/routes";
+import { theme } from "@/app/Theme";
+import { router } from "@/app/routes/Routes";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
