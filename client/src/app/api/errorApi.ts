e268aa370e6dd605612a0456a946c9d642d192e6
Elham-EN <ilhamrezaie@gmail.com>
2025-07-20 20:20:02 +1000

    added error RTK query api to the client

---
+++ client/src/app/api/errorApi.ts
@@ -0,0 +1,32 @@
+import { createApi } from "@reduxjs/toolkit/query/react";
+import { baseQueryWithErrorHandling } from "./baseApi";
+
+export const errorApiSlice = createApi({
+  reducerPath: "errorApi",
+  baseQuery: baseQueryWithErrorHandling,
+  endpoints: (builder) => ({
+    get400Error: builder.query<void, void>({
+      query: () => "/ErrorTest/bad-request",
+    }),
+    get404Error: builder.query<void, void>({
+      query: () => "/ErrorTest/not-found",
+    }),
+    get401Error: builder.query<void, void>({
+      query: () => "/ErrorTest/unauthorized",
+    }),
+    get500Error: builder.query<void, void>({
+      query: () => "/ErrorTest/server-error",
+    }),
+    getValidationError: builder.query<void, void>({
+      query: () => "/ErrorTest/validation-error",
+    }),
+  }),
+});
+
+export const {
+  useLazyGet400ErrorQuery,
+  useGet401ErrorQuery,
+  useGet404ErrorQuery,
+  useLazyGet500ErrorQuery,
+  useLazyGetValidationErrorQuery,
+} = errorApiSlice;
--- client/src/app/store/store.ts
+++ client/src/app/store/store.ts
@@ -1,14 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { catalogApiSlice } from "@/features/catalog/catalogApiSlice";
 import { uiSlice } from "../layout/uiSlice";
+import { errorApiSlice } from "../api/errorApi";
 
 export const store = configureStore({
   reducer: {
     [catalogApiSlice.reducerPath]: catalogApiSlice.reducer,
+    [errorApiSlice.reducerPath]: errorApiSlice.reducer,
     ui: uiSlice.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(catalogApiSlice.middleware),
+    getDefaultMiddleware().concat(
+      catalogApiSlice.middleware,
+      errorApiSlice.middleware
+    ),
 });
 
 export type AppStore = typeof store;
