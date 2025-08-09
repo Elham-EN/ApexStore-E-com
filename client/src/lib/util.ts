c09662961f6694305690b259d5d79af5a5500200
Elham-EN <ilhamrezaie@gmail.com>
2025-08-09 14:22:13 +1000

    created util function to remove empty, null, undefined values and empty array

--- client/src/lib/util.ts
+++ client/src/lib/util.ts
@@ -1,3 +1,20 @@
 export function currencyFormat(amount: number): string {
   return `$` + (amount / 100).toFixed(2);
 }
+
+// Removes empty, null, undefined values and empty arrays
+// from object
+export function filterEmptyValuesFromObj(obj: object): Partial<object> {
+  // Convert Object to Array
+  const arr = Object.entries(obj);
+  // Return Params Values that has value e.g. pageSize=8
+  const filteredArr = arr.filter(
+    ([, value]) =>
+      value !== "" &&
+      value !== null &&
+      value !== undefined &&
+      (!Array.isArray(value) || value.length !== 0)
+  );
+  // Convert Array back to Object
+  return Object.fromEntries(filteredArr);
+}
