e2301e78056bd7c75bca9913351ce2a1bd238054
Elham-EN <ilhamrezaie@gmail.com>
2025-08-14 23:39:44 +1000

    setup .net identity on the API

--- API/Data/StoreContext.cs
+++ API/Data/StoreContext.cs
@@ -1,5 +1,7 @@
 
 using API.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 // "StoreContext = Your database manager with a Products table"
@@ -10,12 +12,31 @@ namespace API.Data
     /// Manages database connections and provides access to tables through DbSets.
     /// Primary constructor - takes database configuration options
     /// Passes it to the parent DbContext class automatically
-    public class StoreContext(DbContextOptions options) : DbContext(options)
+    public class StoreContext(DbContextOptions options) : IdentityDbContext<User>(options)
     {
         /// Represents the Products table in the database.
         /// Use this to query, add, update, or delete products.
         /// Example: _context.Products.ToListAsync() gets all products.
         public required DbSet<Product> Products { get; set; }
-        public required DbSet<Basket>  Baskets{ get; set; }
+        public required DbSet<Basket> Baskets { get; set; }
+        // Customize the model for identity
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<IdentityRole>().HasData(
+                new IdentityRole
+                {
+                    Id = "03bcffa9-233f-4599-bb71-769511f8a74a",
+                    Name = "Member",
+                    NormalizedName = "MEMBER"
+                }, 
+                new IdentityRole
+                {
+                    Id = "5f1f102e-cabd-4259-90fb-e9e7d708a1e3",
+                    Name = "Admin",
+                    NormalizedName = "ADMIN"
+                }
+            );
+        }
     }
 }
---
+++ API/Models/User.cs
@@ -0,0 +1,11 @@
+
+
+using Microsoft.AspNetCore.Identity;
+
+namespace API.Models
+{
+    public class User : IdentityUser
+    {
+        
+    }
+}
