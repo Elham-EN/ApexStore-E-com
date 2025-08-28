89807d53d43c858798e15291911414d035a59f60
Elham-EN <ilhamrezaie@gmail.com>
2025-08-29 01:21:41 +1000

    handle communication between client and Stripe

---
+++ API/Controllers/PaymentsController.cs
@@ -0,0 +1,48 @@
+
+using API.Data;
+using API.DTOs;
+using API.Extensions;
+using API.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace API.Controllers;
+
+public class PaymentsController : BaseApiController
+{
+    private PaymentsService _paymentsService;
+    private StoreContext _storeContext;
+
+    public PaymentsController(PaymentsService paymentsService,
+        StoreContext storeContext)
+    {
+        this._paymentsService = paymentsService;
+        this._storeContext = storeContext;
+    }
+
+    [Authorize]
+    [HttpPost]
+    public async Task<ActionResult<BasketDto>> CreateOrUpdatePaymentIntent()
+    {
+        var basket = await this._storeContext.Baskets.GetBasketWithItems(
+            Request.Cookies["basketId"]
+        );
+
+        if (basket == null) return BadRequest("Can't retrieve the basket");
+
+        var intent = await this._paymentsService.CreateOrUpdatePaymentIntent(basket);
+
+        if (intent == null) return BadRequest("Failed to create payment intent");
+
+        basket.PaymentIntentId ??= intent.Id;
+        basket.ClientSecret ??= intent.ClientSecret;
+
+        var result = await this._storeContext.SaveChangesAsync() > 0;
+
+        if (!result) return BadRequest("Failed to update basket with intent");
+
+        return basket.ToBasketDto();
+    }
+}
+
--- API/Extensions/BasketExtension.cs
+++ API/Extensions/BasketExtension.cs
@@ -1,5 +1,6 @@
 using API.DTOs;
 using API.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Extensions;
 
@@ -11,6 +12,8 @@ public static class BasketExtensions
         return new BasketDto
         {
             BasketId = basket.BasketId,
+            ClientSecret = basket.ClientSecret,
+            PaymentIntentId = basket.PaymentIntentId,
             Items = basket.Items.Select(x => new BasketItemDto
             {
                 ProductId = x.ProductId,
@@ -23,5 +26,19 @@ public static class BasketExtensions
             }).ToList()
 
         };
+    }
+
+    public static async Task<Basket> GetBasketWithItems(this IQueryable<Basket>
+        query, string? basketId)
+    {
+        return await query
+            // Load basket items
+            .Include(x => x.Items)
+            // Load product details for each item
+            .ThenInclude(x => x.Product)
+            // Match basket ID from browser cookie: Uses cookie stored 
+            // in browser to identify which basket belongs to this user
+            .FirstOrDefaultAsync(x => x.BasketId == basketId)
+                ?? throw new Exception("Cannot get basket");
     }
 }   
