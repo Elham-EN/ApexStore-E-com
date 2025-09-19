cb297ddecb6dbb1470d58b7c3231e5037134c1de
Elham-EN <ilhamrezaie@gmail.com>
2025-09-19 22:53:21 +1000

    created endpoint to only allow admin to create new product

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,73 +1,99 @@
 using API.Data;
+using API.DTOs;
 using API.Extensions;
 using API.Models;
 using API.RequestHelpers;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 
-namespace API.Controllers
+namespace API.Controllers;
+
+public class ProductsController : BaseApiController
 {
-    public class ProductsController : BaseApiController
+    private readonly StoreContext _context;
+    private readonly IMapper mapper;
+
+    public ProductsController(StoreContext context, IMapper mapper)
     {
-        private readonly StoreContext _context;
-        public ProductsController(StoreContext context)
-        {
-            _context = context;
-        }
-        [HttpGet]
-        // Route: https://localhost:5001/api/products
-        public async Task<ActionResult<List<Product>>> GetListOfProducts(
-            [FromQuery] ProductParams productParams)
-        {
-            // Create a queryable object from the Products table in the 
-            // database. This allows us to build and chain query operations 
-            // (like filtering, sorting) before actually executing the 
-            // database query for better performance.
-            // Deferred execution: The query doesn't run immediately - 
-            // you can add more conditions first
-            var query = _context.Products
-                .Sort(productParams.OrderBy)
-                .Search(productParams.SearchTerm)
-                .Filter(productParams.Brands, productParams.Types)
-                .AsQueryable();
-
-            var products = await PagedList<Product>.ToPagedList(query,
-                productParams.PageNumber, productParams.Pagesize);
-
-            Response.AddPaginationHeader(products.Metadata);
-
-            return products;
-        }
-        // Route: https://localhost:5001/api/products/2
-        [HttpGet("id")] // id => route parameter
-        public async Task<ActionResult<Product>> GetSingleProduct(int id)
-        {
-            // Finds an entity with the given primary key values
-            var product = await _context.Products.FindAsync(id);
-
-            // If product do not exist then return notfound 404
-            if (product == null) return NotFound();
-
-            return product;
-        }
-
-        [HttpGet("filters")]
-        public async Task<IActionResult> GetFilters()
-        {
-            var brands = await _context.Products
-                .Select(x => x.Brand)
-                // Remove duplicate brand names, keep only unique ones
-                .Distinct()
-                .ToListAsync();
-
-            var types = await _context.Products
-                .Select(x => x.Type)
-                // Remove duplicate brand names, keep only unique ones
-                .Distinct()
-                .ToListAsync();
-
-            return Ok(new {brands, types});
-        }
+        this._context = context;
+        this.mapper = mapper;
     }
-} 
+    
+    [HttpGet]
+    // Route: https://localhost:5001/api/products
+    public async Task<ActionResult<List<Product>>> GetListOfProducts(
+        [FromQuery] ProductParams productParams)
+    {
+        // Create a queryable object from the Products table in the 
+        // database. This allows us to build and chain query operations 
+        // (like filtering, sorting) before actually executing the 
+        // database query for better performance.
+        // Deferred execution: The query doesn't run immediately - 
+        // you can add more conditions first
+        var query = _context.Products
+            .Sort(productParams.OrderBy)
+            .Search(productParams.SearchTerm)
+            .Filter(productParams.Brands, productParams.Types)
+            .AsQueryable();
+
+        var products = await PagedList<Product>.ToPagedList(query,
+            productParams.PageNumber, productParams.Pagesize);
+
+        Response.AddPaginationHeader(products.Metadata);
+
+        return products;
+    }
+    // Route: https://localhost:5001/api/products/2
+    [HttpGet("id")] // id => route parameter
+    public async Task<ActionResult<Product>> GetSingleProduct(int id)
+    {
+        // Finds an entity with the given primary key values
+        var product = await _context.Products.FindAsync(id);
+
+        // If product do not exist then return notfound 404
+        if (product == null) return NotFound();
+
+        return product;
+    }
+
+    [HttpGet("filters")]
+    public async Task<IActionResult> GetFilters()
+    {
+        var brands = await _context.Products
+            .Select(x => x.Brand)
+            // Remove duplicate brand names, keep only unique ones
+            .Distinct()
+            .ToListAsync();
+
+        var types = await _context.Products
+            .Select(x => x.Type)
+            // Remove duplicate brand names, keep only unique ones
+            .Distinct()
+            .ToListAsync();
+
+        return Ok(new { brands, types });
+    }
+    
+    [Authorize(Roles = "Admin")]
+    [HttpPost]
+    public async Task<ActionResult<Product>> CreateProduct(
+        CreateProductDto productDto)
+    {
+        var product = this.mapper.Map<Product>(productDto);
+
+        this._context.Products.Add(product);
+
+        var result = await this._context.SaveChangesAsync() > 0;
+
+        if (result)
+        {
+            return CreatedAtAction(nameof(GetSingleProduct), new { Id = product.Id }, product);
+        }
+
+        return BadRequest("Problem creating new product");
+    }
+}
+
